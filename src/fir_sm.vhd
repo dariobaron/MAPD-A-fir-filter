0e4edcffefa249be9760c428aa326b2e69481a9c
dariobaron <barone.dario97@gmail.com>
2021-03-20 10:18:39 +0100

    hopefully everything works

--- src/fir_sm.vhd
+++ src/fir_sm.vhd
@@ -4,11 +4,11 @@ use ieee.numeric_std.all;
 
 entity fir_filter is
 	port(
-		clk:	in std_logic;
+		clock:	in std_logic;
 		valid:	in std_logic;
 		data :	in std_logic_vector(7 downto 0);
 
-		valid_out:	out std_logic;
+		data_valid:	out std_logic;
 		data_out:	out std_logic_vector(7 downto 0));
 
 end entity fir_filter;
@@ -18,17 +18,14 @@ architecture str of fir_filter is
 	type state_t is (s0,s1,s2,s3,s4);
 	signal state :	state_t := s0;
 
-	type arr8_t is array (0 to 4) of signed(7 downto 0);
-	type arr16_t is array (0 to 4) of signed(15 downto 0);
+	type arr8_t is array (0 to 5) of signed(7 downto 0);
+	type arr16_t is array (0 to 5) of signed(15 downto 0);
 	 
 
 	signal data_pipe :	arr8_t := (others => (others => '0'));
 	signal coeff :	arr8_t;
 	signal mult : arr16_t := (others => (others => '0'));
-	signal adder : signed(20 downto 0) := (others => '0'); 
-
-  signal counter   : unsigned(9 downto 0) := (others => '0');
-  constant divisor : unsigned(9 downto 0) := to_unsigned(15, 10);
+	signal adder : signed(20 downto 0) := (others => '0');
 
 begin  -- architecture
 
@@ -37,67 +34,67 @@ begin  -- architecture
       --coeff(2) <= to_signed(2,8);
       --coeff(3) <= to_signed(3,8);
 
-coeff(0) <= "00010001";
-coeff(1) <= "00110101";
-coeff(2) <= "00010010";
-coeff(3) <= "00110110";
-coeff(4) <= "00110110";
+coeff(0) <= "00000010";
+coeff(1) <= "00001010";
+coeff(2) <= "00010110";
+coeff(3) <= "00010110";
+coeff(4) <= "00001010";
+coeff(5) <= "00000010";
 
-  main : process (clk,valid) is
+  main : process (clock,valid) is
   begin  -- process main
-    if rising_edge(clk) then -- rising clock edge
+    if rising_edge(clock) then -- rising clock edge
       
       case state is
 
         when s0 =>     --initialized to s0 state
-          valid_out  <= '0';
+          data_valid  <= '0';
           if valid = '1' then
             state <= s1;
           end if;
 
         when s1 =>
-          valid_out  <= '0';
-	  data_pipe  <= signed(data)&data_pipe(0 to data_pipe'length-2);
+        	data_valid  <= '0';
+	  		data_pipe  <= signed(data)&data_pipe(0 to data_pipe'length-2);
 
-          if valid = '0' then
-              state <= s2;
-          end if;
+        	if valid = '0' then
+        		state <= s2;
+        	end if;
 	  
         when s2 =>
-	  --counter <= counter + 1;
+  			  mult(0) <= data_pipe(0)*coeff(0);
+			  mult(1) <= data_pipe(1)*coeff(1);
+			  mult(2) <= data_pipe(2)*coeff(2);
+			  mult(3) <= data_pipe(3)*coeff(3);
+			  mult(4) <= data_pipe(4)*coeff(4);
+			  mult(5) <= data_pipe(5)*coeff(5);
 
-  	  mult(0) <= data_pipe(0)*coeff(0);
-	  mult(1) <= data_pipe(1)*coeff(1);
-	  mult(2) <= data_pipe(2)*coeff(2);
-	  mult(3) <= data_pipe(3)*coeff(3);
-	  mult(4) <= data_pipe(4)*coeff(4);
+			  if valid = '0' then
+            	  state <= s3;
+        	  end if;
 
+		when s3 =>
+		  adder <= 	(resize(mult(0),21) + resize(mult(1),21) + resize(mult(2),21) +
+					resize(mult(3),21) + resize(mult(4),21) + resize(mult(5),21));
 
-         --if counter = divisor then
-	  if valid = '0' then
-              state <= s3;
-	   --   counter <= (others => '0');
-          end if;
-
-	when s3 =>
-	  adder <= 	(resize(mult(0),21) + resize(mult(1),21) + resize(mult(2),21) +
-			resize(mult(3),21) + resize(mult(4),21));
-
-	  if valid = '0' then
+		  if valid = '0' then
               state <= s4;
           end if;
 
 
-	when s4 =>
-	  data_out <=	std_logic_vector(adder(20 downto 13));--shift rightresize(adder, 8)
-          valid_out  <= '1';
-	  if valid = '0' then
-              state <= s0;
-          end if;
+		when s4 =>
+			  data_out <=	std_logic_vector(adder(20 downto 13));--shift rightresize(adder, 8)
+    	      data_valid  <= '1';
+			  if valid = '0' then
+        	      state <= s0;
+         	  end if;
 
         when others => null;
       end case;
+
     end if;
+
   end process main;
+
 end architecture str;
 
--- src/top.vhd
+++ src/top.vhd
@@ -14,20 +14,12 @@ entity top is
 end entity top;
 
 architecture str of top is
-  signal clock        : std_logic;
-  signal data_to_send : std_logic_vector(7 downto 0) := X"61";
-  signal data_valid   : std_logic;
-  signal busy         : std_logic;
-  signal uart_tx      : std_logic;
-
-  component uart_transmitter is
-    port (
-      clock        : in  std_logic;
-      data_to_send : in  std_logic_vector(7 downto 0);
-      data_valid   : in  std_logic;
-      busy         : out std_logic;
-      uart_tx      : out std_logic);
-  end component uart_transmitter;
+  signal clock         : std_logic;
+  signal valid         : std_logic;
+  signal received_data : std_logic_vector(7 downto 0);
+  signal data_valid    : std_logic;
+  signal data_to_send  : std_logic_vector(7 downto 0);
+  signal busy          : std_logic;
 
   component uart_receiver is
     port (
@@ -36,6 +28,24 @@ architecture str of top is
       valid         : out std_logic;
       received_data : out std_logic_vector(7 downto 0));
   end component uart_receiver;
+
+  component fir_filter is
+    port (
+      clock         : in std_logic;
+      valid         : in std_logic;
+      data          : in std_logic_vector(7 downto 0);
+      data_valid    : out std_logic;
+      data_out      : out std_logic_vector(7 downto 0));
+  end component fir_filter;
+
+  component uart_transmitter is
+    port (
+      clock         : in  std_logic;
+      data_to_send  : in  std_logic_vector(7 downto 0);
+      data_valid    : in  std_logic;
+      busy          : out std_logic;
+      uart_tx       : out std_logic);
+  end component uart_transmitter;
 begin  -- architecture str
 
   uart_receiver_1 : uart_receiver
@@ -43,8 +53,18 @@ begin  -- architecture str
     port map (
       clock         => CLK100MHZ,
       uart_rx       => uart_txd_in,
-      valid         => data_valid,
-      received_data => data_to_send);
+      valid         => valid,
+      received_data => received_data);
+
+  fir_filter_1 : fir_filter
+
+    port map(
+      clock         => CLK100MHZ,
+      valid         => valid,
+      data          => received_data,
+      data_valid    => data_valid,
+      data_out      => data_to_send);
+
   uart_transmitter_1 : uart_transmitter
     port map (
       clock        => CLK100MHZ,
