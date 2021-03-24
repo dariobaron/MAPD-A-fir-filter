6859ad2e2ae33cb89419aaf038e5cb6e1a8084ca
LEONELLI Elena 1151814-SF <leonelle@lxilinx1.fisica.unipd.it>
2021-03-24 10:35:47 +0100

    new signal

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import os
 import serial
 import numpy as np
 from numpy import log2
-
+import time
 
 def computeQ(y,taps):
     M = max(max(np.abs(taps)),max(np.abs(y)))
@@ -25,7 +25,7 @@ def signed_to_double(a,Q):
 ser = serial.Serial('/dev/ttyUSB5', baudrate=115200)
 
 
-######## CHECK
+####### CHECK
 taps = np.array([0.02856076, 0.14288273, 0.32855651, 0.32855651, 0.14288273, 0.02856076])
 
 
@@ -35,13 +35,19 @@ with open("signal.txt") as f, open("filtered_signal.txt", "w") as out:
     Q = computeQ(signal,taps)
 
     for sig in signal:
-#    	print("Processing : "+str(100*i//signal.size)+"%", end='\r', flush=True)
-        ser.write(chr(double_to_signed(sig,Q=Q)))
-        #print(sig)
-        d = ser.read()
-        res = signed_to_double(ord(d),Q=Q) * max(signal)
-        out.write(str(res) + '\n' )
+	#print("Processing : "+str(100*i//signal.size)+"%", end='\r', flush=True)
+      
+	ser.write(chr(double_to_signed(sig,Q=Q)))
+        print(sig)
 
+	
+	if(ser.inWaiting()>0):
+	       	d = ser.read()
+        	res = signed_to_double(ord(d),Q=Q) * max(signal)
+        
+		out.write(str(res) + '\n' )
+		time.sleep(0.01)
     f.close()
     out.close()
 ser.close() # close port
+
