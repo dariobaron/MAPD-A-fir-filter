3598159e82bf4b58de8fab990bae0c3a2dc63543
dariobaron <barone.dario97@gmail.com>
2021-04-10 01:09:51 +0200

    added script 4

---
+++ script3.py
@@ -0,0 +1,71 @@
+import os
+import serial
+import numpy as np
+from numpy import log2
+import time
+
+def computeQ(y,taps):
+    M = max(max(np.abs(taps)),max(np.abs(y)))
+    Q = log2(127) - log2(M)
+    return Q
+
+def double_to_signed(a, Q):
+    int_a = np.round(a*2**Q)
+    signed = np.where(int_a >= 0, int_a, int_a + 256).astype(int)
+    return signed
+
+def signed_to_double(a,Q):
+    signed = np.where(a >= 128, a-256, a)
+    double_a = signed*2**(-Q)
+    return double_a
+
+
+
+
+#ser = serial.Serial('/dev/ttyUSB5', baudrate=115200)
+
+
+####### CHECK
+taps = np.array([0.02856076, 0.14288273, 0.32855651, 0.32855651, 0.14288273, 0.02856076])
+
+
+
+with open("signal.txt") as f, open("filtered_signal.txt", "w") as out:
+    signal = np.loadtxt(f)
+    Q = computeQ(signal,taps)
+
+
+    with serial.Serial('/dev/ttyUSB5', baudrate=115200, timeout=2) as ser:
+        print "Opening serial port..."
+        time.sleep(2)
+        print "Opened."
+        for i,sig in enumerate(signal):
+    	    print "Processing : ", 100*i//signal.size, "%\r",
+            bytes_written = ser.write(chr(double_to_signed(sig,Q=Q)))
+            time.sleep(0.1)
+            d = ser.read(bytes_written)
+#            print bytes_written, ser.in_waiting, ser.out_waiting, len(d)
+            res = signed_to_double(ord(d),Q=Q) * max(signal)
+            out.write(str(res) + '\n' )
+
+#    f.close()
+#    out.close()
+#ser.close() # close port
+"""
+    for sig in signal:
+	#print("Processing : "+str(100*i//signal.size)+"%", end='\r', flush=True)
+      
+	ser.write(chr(double_to_signed(sig,Q=Q)))
+        print(sig)
+
+	
+	if(ser.inWaiting()>0):
+	       	d = ser.read()
+        	res = signed_to_double(ord(d),Q=Q) * max(signal)
+        
+		out.write(str(res) + '\n' )
+		time.sleep(0.01)
+    f.close()
+    out.close()
+ser.close() # close port
+"""
---
+++ script4.py
@@ -0,0 +1,20 @@
+import os
+import serial
+import numpy as np
+from numpy import log2
+import time
+
+for i in range(16):
+	ser = serial.Serial('/dev/ttyUSB5', baudrate=115200)
+	time.sleep(2)
+
+	with open("signal"+str(i)".txt") as f, open("filtered_signal"+str(i)".txt","w") as out:
+	    signal = np.loadtxt(f,dtype=int)
+
+	    for j,sig in enumerate(signal):
+			ser.write(chr(sig))
+			d= ser.read()
+			print "Completed", 100*j//signal.size, "%\r",
+			out.write(str(ord(d))+"\n")
+	ser.close() # close port
+	print "Processed file :", i
