7307bd1afccab6b5cb719b6cbb97f121e8263093
dariobaron <barone.dario97@gmail.com>
2021-04-16 00:05:31 +0200

    fixed compatibility issues

--- script3.py
+++ script3.py
@@ -4,8 +4,8 @@ import numpy as np
 from numpy import log2
 import time
 
-def computeQ(y,taps):
-    M = max(max(np.abs(taps)),max(np.abs(y)))
+def computeQ(y):
+    M = max(np.abs(y))
     Q = log2(127) - log2(M)
     return Q
 
@@ -20,19 +20,9 @@ def signed_to_double(a,Q):
     return double_a
 
 
-
-
-#ser = serial.Serial('/dev/ttyUSB5', baudrate=115200)
-
-
-####### CHECK
-taps = np.array([0.02856076, 0.14288273, 0.32855651, 0.32855651, 0.14288273, 0.02856076])
-
-
-
 with open("signal.txt") as f, open("filtered_signal.txt", "w") as out:
     signal = np.loadtxt(f)
-    Q = computeQ(signal,taps)
+    Q = computeQ(signal)
 
 
     with serial.Serial('/dev/ttyUSB5', baudrate=115200, timeout=2) as ser:
@@ -42,11 +32,11 @@ with open("signal.txt") as f, open("filtered_signal.txt", "w") as out:
         for i,sig in enumerate(signal):
     	    print "Processing : ", 100*i//signal.size, "%\r",
             bytes_written = ser.write(chr(double_to_signed(sig,Q=Q)))
-            time.sleep(0.1)
             d = ser.read(bytes_written)
-#            print bytes_written, ser.in_waiting, ser.out_waiting, len(d)
             res = signed_to_double(ord(d),Q=Q) * max(signal)
             out.write(str(res) + '\n' )
+
+print "Done."
 
 #    f.close()
 #    out.close()
