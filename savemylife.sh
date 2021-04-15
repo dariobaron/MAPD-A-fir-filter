d7e98c4c20282eb9bffe75af9096fc956fbaf2f5
dariobaron <baroned@lxilinx1.fisica.unipd.it>
2021-04-15 17:47:00 +0200

    other results

--- savemylife.sh
+++ savemylife.sh
@@ -4,9 +4,12 @@ set -v
 
 source vivado_activation.sh
 
+make clean && make && make program_fpga
+sleep 20
+
 for i in `seq 0 15`; do
-	make clean && make && make program_fpga
-	sleep 10
+#	make clean && make && make program_fpga
+	sleep 20
 	python script5.py $i
-	sleep 10
+	sleep 20
 done
